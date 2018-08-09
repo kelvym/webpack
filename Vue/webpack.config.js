0fdc3619435724c451bc9becd8b8066cb477fd3f
Kelvym Miranda <kelvymm@gmail.com>
2018-08-09 14:33:18 -0300

    Update webpack.config.js

--- Vue/webpack.config.js
+++ Vue/webpack.config.js
@@ -1,1 +1,46 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?cacheDirectory'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: '[path][name].[ext]',
+                    outputPath: 'images/'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'screen.css'
+        })
+    ]
+}
